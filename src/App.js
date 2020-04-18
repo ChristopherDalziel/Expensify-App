5d497bbebd37c7acc04c8f5140046ff8b2d3519b
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-04-18 19:34:02 +1000

    Moved routes to their own component folder

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
 import React from "react";
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-import ExpenseDashboardPage from "./components/pages/ExpenseDashboardPage";
-import AddExpensePage from "./components/pages/AddExpensePage";
-import EditExpensePage from "./components/pages/EditExpensePage";
-import HelpPage from "./components/pages/HelpPage";
-import NotFoundPage from "./components/pages/NotFoundPage";
-
-import Header from "./components/Header";
+import AppRouter from "./routers/AppRouter";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={ExpenseDashboardPage} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
-
-        <Route component={NotFoundPage} />
-      </Switch>
-    </BrowserRouter>
-  );
+  return <AppRouter />;
 };
 
 export default App;
