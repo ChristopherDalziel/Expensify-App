d1473b3333a2b1158a32e252cc84826e79a7778d
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-04-18 19:12:11 +1000

    Added to Header/Nav toNavLinks and basic css file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,24 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
-import { Link } from "react-router-dom";
+import "../styles/header.css";
 
 const Header = () => {
   return (
     <div>
       <h1>Expensify</h1>
-      <Link to="/">Dashboard</Link>
-      <Link to="/create">Create</Link>
-      <Link to="/edit">Edit</Link>
-      <Link to="/help">Help</Link>
+      <NavLink exact activeClassName="isActive" to="/">
+        Dashboard
+      </NavLink>
+      <NavLink activeClassName="isActive" to="/create">
+        Create
+      </NavLink>
+      <NavLink activeClassName="isActive" to="/edit">
+        Edit
+      </NavLink>
+      <NavLink activeClassName="isActive" to="/help">
+        Help
+      </NavLink>
     </div>
   );
 };
