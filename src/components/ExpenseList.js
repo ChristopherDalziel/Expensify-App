7d3f563632f8c734b0a7652acd0bfe412cf70704
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-04-23 23:39:55 +1000

    created my first HOD component that recieves props from the store

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { connect } from "react-redux";
+
+// Regular Component
+const ExpenseList = (props) => (
+  <div>
+    <h1>Expense List</h1>
+    {props.filters.text} <br />
+    {props.expenses.length}
+  </div>
+);
+
+// Function
+const mapStateToProps = (state) => {
+  return {
+    expenses: state.expenses,
+    filters: state.filters,
+  };
+};
+
+// Higher order component
+// export default connect((state) => {
+//   return {
+//     expenses: state.expenses,
+//   };
+// })(ExpenseList);
+
+// Connect call that pulls our data together, the result of this is a brand new component which is our component with the props from the store
+export default connect(mapStateToProps)(ExpenseList);
--- src/components/pages/ExpenseDashboardPage.js
+++ src/components/pages/ExpenseDashboardPage.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import ExpenseList from "../ExpenseList";
+
 const ExpenseDashboardPage = () => {
-  return <div>Dashboard Page</div>;
+  return (
+    <div>
+      <ExpenseList />
+    </div>
+  );
 };
 
 export default ExpenseDashboardPage;
