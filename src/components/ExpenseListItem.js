490a36d57bac61476843b4a391a7fd62bec73c21
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-04-24 21:47:44 +1000

    Created a button that allows us to remove an individual item from the store

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
 import { removeExpense } from "../actions/expenses";
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ dispatch, description, amount, createdAt, id }) => (
   <div>
     <h3>{description}</h3>
     <p>
       {amount} - {createdAt}
     </p>
-    <button>Remove</button>
+    <button
+      onClick={(e) => {
+        dispatch(removeExpense({ id }));
+      }}
+    >
+      Remove
+    </button>
   </div>
 );
 
-export default ExpenseListItem;
+const mapStateToProps = (state) => {
+  return {
+    expenses: state.expenses,
+  };
+};
+
+export default connect()(ExpenseListItem);
