a6f811cee3f07079fc572aebe7331240903f41cf
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-04-25 21:49:44 +1000

    Created a new form, as well as two onChange functions that allow our local state to track the inputs within the form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,7 +1,43 @@
 import React from "react";
 
 export default class ExpenseForm extends React.Component {
+  state = {
+    description: "",
+    note: "",
+  };
+
+  onDescriptionChange = (e) => {
+    const description = e.target.value;
+    this.setState(() => {
+      return { description };
+    });
+  };
+
+  onTextAreChange = (e) => {
+    const note = e.target.value;
+    this.setState(() => {
+      return { note };
+    });
+  };
+
   render() {
-    return <div>Expense Form</div>;
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Description"
+          autoFocus
+          value={this.state.description}
+          onChange={this.onDescriptionChange}
+        ></input>
+        <input type="number" placeholder="Amount"></input>
+        <textarea
+          placeholder="Add a note for your expense (optional)"
+          value={this.state.note}
+          onChange={this.onTextAreChange}
+        ></textarea>
+        <button>Add Expense</button>
+      </form>
+    );
   }
 }
