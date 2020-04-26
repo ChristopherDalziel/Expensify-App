cc856501e14c239d309b848fdd6aae1558c7efdc
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-04-26 22:00:14 +1000

    When clicking edit, recieve a matched object as well as rendering the ExpenseForm to the screen

--- src/components/pages/EditExpensePage.js
+++ src/components/pages/EditExpensePage.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
+import ExpenseForm from "../ExpenseForm";
 
 const EditExpensePage = (props) => {
+  console.log(props);
   return (
     <>
       <h1>Edit Expense</h1>
-      <p>Editing expense {props.match.params.id}</p>
+      <ExpenseForm
+        onSubmit={(expense) => {
+          console.log("Updated", expense);
+        }}
+      />
     </>
   );
 };
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find((expense) => {
+      return expense.id === props.match.params.id;
+    }),
+  };
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
