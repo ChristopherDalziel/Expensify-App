e632056d83440093740f1d7cbcf3ef84389de797
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-04-26 21:23:06 +1000

    Using the addExpense action to add our new expense to local state, then onSubmit redirects us from the create expense page to the dashboard where we can see our created expense

--- src/components/pages/AddExpensePage.js
+++ src/components/pages/AddExpensePage.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
 import ExpenseForm from "../ExpenseForm";
+import { addExpense } from "../../actions/expenses";
 
-const AddExpensePage = () => {
+const AddExpensePage = (props) => {
   return (
     <div>
       <h1>Add Expense</h1>
       <ExpenseForm
         onSubmit={(expense) => {
-          console.log(expense);
+          // The addExpense function we set up earlier takes an object with all of the values that the expense prop we're passing up is holding.
+          props.dispatch(addExpense(expense));
+          props.history.push("/");
         }}
       />
     </div>
   );
 };
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
