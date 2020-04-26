3f65fd4cfa15182481d185bed6cfa6911df1d91c
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-04-26 22:35:28 +1000

    Added line for redux dev tools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,7 +4,8 @@ import filterReducer from "../reducers/filters";
 
 export default () => {
   const store = createStore(
-    combineReducers({ expenses: expensesReducer, filters: filterReducer })
+    combineReducers({ expenses: expensesReducer, filters: filterReducer }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
   return store;
