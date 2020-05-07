d6b37a7326759fefb0a48085c60fbd75de4e178c
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-05-07 21:33:14 +1000

    Changed location of setupTest file as it needs to be in src to run, not __tests__ sort

--- src/__tests__/components/Header.test.js
+++ src/__tests__/components/Header.test.js
@@ -1,10 +1,13 @@
 import React from "react";
-import ReactShallowRenderer from "react-test-renderer/shallow";
+// import ReactShallowRenderer from "react-test-renderer/shallow";
+import { shallow } from "enzyme";
 import Header from "../../components/Header";
 // import { render } from "@testing-library/react";
 
 test("Should render header correctly", () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(wrapper.find("h1").length).toBe(1);
+  // const renderer = new ReactShallowRenderer();
+  // renderer.render(<Header />);
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
