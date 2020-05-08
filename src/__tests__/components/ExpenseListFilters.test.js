34bcf9b2ea39fe6d0759f1aca2f1fe28620538ef
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-05-08 23:08:58 +1000

    Added two expense list filters tests, one to ensure it was displaying correctly with our default filters, and one with a set of alt filters we loaded in by using setProps

--- src/__tests__/components/ExpenseListFilters.test.js
+++ src/__tests__/components/ExpenseListFilters.test.js
@@ -1,7 +1,35 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { ExpenseListFilters } from "../../components/ExpenseListFilters";
+import { filters, altFilters } from "../fixtures/filters";
 
 import toJSON from "enzyme-to-json";
 
-test("", () => {});
+let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
+
+beforeEach(() => {
+  setTextFilter = jest.fn();
+  sortByDate = jest.fn();
+  sortByAmount = jest.fn();
+  setStartDate = jest.fn();
+  setEndDate = jest.fn();
+  wrapper = shallow(
+    <ExpenseListFilters
+      filters={filters}
+      setTextFilter={setTextFilter}
+      sortByDate={sortByDate}
+      sortByAmount={sortByAmount}
+      setStartDate={setStartDate}
+      setEndDate={setEndDate}
+    />
+  );
+});
+
+test("Should render ExpenseListFilters correctly", () => {
+  expect(toJSON(wrapper)).toMatchSnapshot();
+});
+
+test("Should render ExpenseListFilters with alt data correctly", () => {
+  wrapper.setProps({ filters: altFilters });
+  expect(toJSON(wrapper)).toMatchSnapshot();
+});
