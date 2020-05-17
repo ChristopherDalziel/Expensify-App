dc1664683cbfcf0f161b79e567e5751eea8a9782
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-05-17 18:19:07 +1000

    finished final component displaying a summary of amount of expenses paid and their total

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,17 +1,27 @@
 import React from "react";
-import { connect } from "react-dom";
+import { connect } from "react-redux";
 import numeral from "numeral";
+import getVisibleExpenses from "../selectors/expenses";
+import selectExpensesTotal from "../selectors/expensesTotal";
 
 const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
-  const expenseWord = expenseCount === 0 ? "expense" : "expenses";
+  const expenseWord = expenseCount === 1 ? "expense" : "expenses";
   const formattedExpensesTotal = numeral(expensesTotal / 100).format("$0,0.00");
   return (
     <div>
       <h1>
-        Viewing {expenseCount} {expenseWord} totalling ${formattedExpensesTotal}
+        Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}
       </h1>
     </div>
   );
 };
 
-export default ExpensesSummary;
+const mapStateToProps = (state) => {
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+  return {
+    expenseCount: visibleExpenses.length,
+    expensesTotal: selectExpensesTotal(visibleExpenses),
+  };
+};
+
+export default connect(mapStateToProps)(ExpensesSummary);
