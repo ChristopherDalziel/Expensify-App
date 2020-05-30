b963c2269d7dc29a86269d09bd6940b445169fef
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-05-30 16:33:03 +1000

    Added public routing

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,11 +5,11 @@ import { Router, Route, Switch } from "react-router-dom";
 import ExpenseDashboardPage from "../components/pages/ExpenseDashboardPage";
 import AddExpensePage from "../components/pages/AddExpensePage";
 import EditExpensePage from "../components/pages/EditExpensePage";
-import HelpPage from "../components/pages/HelpPage";
 import NotFoundPage from "../components/pages/NotFoundPage";
 import LoginPage from "../components/pages/LoginPage";
 
 import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 
 export const history = createHistory();
 
@@ -19,11 +19,10 @@ const AppRouter = () => {
     // Now using Router instead of BrowserRouter we're allowed to give our own router value, in our case it will be the history variable we just created and now we can use it within other files.
     <Router history={history}>
       <Switch>
-        <Route exact path="/" component={LoginPage} />
+        <PublicRoute exact path="/" component={LoginPage} />
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
         <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
 
         <Route component={NotFoundPage} />
       </Switch>
