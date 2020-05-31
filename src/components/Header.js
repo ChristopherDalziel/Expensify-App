41639cd30e8ed83049a0d7f7bd26036d26a7c8b9
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-05-31 20:06:11 +1000

    updated colours on Heading

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,16 @@ import { connect } from "react-redux";
 import { startLogout } from "../actions/auth";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import styled from "@emotion/styled";
+
+const NavBarHeading = styled.h1`
+  color: white;
+`;
 
 export const Header = ({ startLogout }) => {
   return (
-    <Navbar>
-      <h1>Expensify</h1>
+    <Navbar bg="dark">
+      <NavBarHeading>Expensify</NavBarHeading>
       <Nav.Item>
         <Nav.Link as={NavLink} exact activeClassName="isActive" to="/dashboard">
           Dashboard
