429f7fa9537304e0dfdb6515dba41bb7673e8c47
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-05-31 20:11:50 +1000

    Centered login buton and updated snapshot accordingly

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -2,14 +2,22 @@ import React from "react";
 import { connect } from "react-redux";
 import { startLogin } from "../../actions/auth";
 import Button from "react-bootstrap/Button";
+import styled from "@emotion/styled";
+
+const LoginContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const LoginPage = ({ startLogin }) => {
   return (
-    <div>
+    <LoginContainer>
       <Button variant="outline-primary" onClick={startLogin}>
         Login
       </Button>
-    </div>
+    </LoginContainer>
   );
 };
 
