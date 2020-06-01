914182156d187596b2483538a8e9d4145cfc55c9
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-06-01 13:48:03 +1000

    Updated expenselistitem as well as snapshots, and removed <>

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,19 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
 import numeral from "numeral";
+import styled from "@emotion/styled";
+
+const ExpenseListItemContainer = styled.div`
+  margin: 10px;
+`;
 
 const ExpenseListItem = ({ description, amount, createdAt, id }) => (
-  <div>
-    <>
-      <Link to={`/edit/${id}`}>
-        <h3>{description}</h3>
-      </Link>
-      <p>
-        {numeral(amount / 100).format("$0,0.00")} -
-        {moment(createdAt).format("MMMM Do, YYYY")}
-      </p>
-    </>
-  </div>
+  <ExpenseListItemContainer>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
+    <p>
+      {numeral(amount / 100).format("$0,0.00")} -
+      {moment(createdAt).format("MMMM Do, YYYY")}
+    </p>
+  </ExpenseListItemContainer>
 );
 
 export default ExpenseListItem;
